471203c6d7be4b9e33f5dee53795a8d3d69a926a
malinvaudpaul <paulmalinvaud@proton.me>
2023-01-18 16:42:16 +0100

    add basic charts

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
+import { Data } from "./utils/Data";
 import './App.css';
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 function App() {
   return (
     <div className="">
-      <h2 id="data">Data visualisation </h2>
-      <form id="visual">
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Saississez l'id d'un utilisateur</label>
-        <input type="text" class="form-control" id="idUser" name="idUser" />
-      </div><br/>
-      <button type="submit" class="btn btn-primary">Visualiser</button>
-      </form>
+      <div>
+        <h2 id="data">Data visualisation </h2>
+        <form id="visual">
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Saississez l'id d'un utilisateur</label>
+          <input type="text" class="form-control" id="idUser" name="idUser" />
+        </div><br/>
+        <button type="submit" class="btn btn-primary">Visualiser</button>
+        </form>
+      </div>
+      <BarChart />
+      <LineChart />
+      <PieChart />
     </div>
   );
 }
