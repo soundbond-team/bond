7edc2fd40fdc4c83d8f73c3e0b43322623497ca4
Maguitadiop <diopma221@gmail.com>
2023-01-19 21:08:56 +0100

    add lineChart

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,27 +1,65 @@
 import React from "react";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
+import axios from "axios";
 
-const labels = ["January", "February", "March", "April", "May", "June"];
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
-    },
-  ],
-};
+const BarChart = () => {
 
-const LineChart = () => {
+  const [dataset, setDataset] = React.useState([{}]);
+  const labels = ["Following", "Media", "Follow"];
+  let follwCount = 0;
+  let mediaCount = 0;
+  let followingCount = 0;
+  const PYTHON_SERVER_URL = "http://localhost:8000"
+
+  React.useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await axios.get(`${PYTHON_SERVER_URL}/users`);
+        return res.data;
+      }
+      catch (err) {
+        console.log(err);
+      }
+    };
+    const data = () => {
+      getUsers().then((res) => {
+        setDataset(res);
+      })
+    };
+    
+    data();
+  }, []);
+
+  dataset.map((user) =>{
+      if(user.user_media_count){
+          mediaCount += user.user_media_count;
+     }
+      if(user.user_follower_count){
+          follwCount += user.user_follower_count;
+      }
+      if(user.user_following_count){
+          followingCount += user.user_following_count;
+      }
+  })
+  const finalData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Nombre total",
+        backgroundColor: ["#CD853F"],
+        borderColor: "rgb(255, 99, 132)",
+        data: [followingCount, mediaCount, follwCount]
+      },
+    ],
+  };
+  
   return (
     <div>
-      <Line data={data} />
+      <Line data={finalData}/>
     </div>
   );
 };
 
-export default LineChart;
+export default BarChart;
