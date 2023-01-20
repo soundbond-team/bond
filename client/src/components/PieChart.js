5d0e085346f12a04391b050365a712bb1de61dce
cathy <cathy.siwatala@gmail.com>
2023-01-20 23:08:22 +0100

    Double doughnut chart

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -131,70 +131,95 @@ const PieChart = () => {
   console.log(S1)
   console.log(S2)
 
+  var option1 = {
+    tooltips: {
+      enabled:false
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value) => {
+          let pourcentage = (value*100/S1).toFixed(2)+"%";
+          return pourcentage;
+        },
+      },
+    },
+  }
+
+  var option2 = {
+    tooltips: {
+      enabled:false
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value) => {
+          let pourcentage = (value*100/S2).toFixed(2)+"%";
+          return pourcentage;
+        },
+      },
+    },
+  }
+
+
 
 
   const finalData = {
     labels: [
-      'faux',
-      'vrai',
-      'S1',
-      'S2'
+      '0 chiffres',
+      '1-2 chiffres',
+      '3-4 chiffres',
+      '5-6 chiffres',
+      '7-8 chiffres',
+      '9-10 chiffres'
     ],
     datasets: [
-      {
-        type: 'doughnut',
-        label: ' AccountsCount',
-        data: [nbrFaux, nbrVrai],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      {
-        type: 'pie',
-        label: ' Vraix/faux compte',
-        data: [nbrFaux, nbrVrai],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      'rgb(255, 255, 128)',
-      'rgb(255, 30, 50)',
-      'rgb(255, 20, 50)',
-        ],
-        hoverOffset: 4
-      },
-        ],
-        hoverOffset: 4
-      },
-   
+     
       {
         type:'doughnut',
-        label:'DigitNumber sachant Vrai/faux',
-        data:[S2,S1],
+        label:'Vrai compte, valeur',
+        data:[g1E1,g2E1,g3E1,g4E1,g5E1,g6E1],
         backgroundColor: [
-          'rgb(25, 30, 50)',
-          'rgba(2, 255, 128, .5)',
+          'rgb(255, 99, 132)', /*rose */
+      'rgb(54, 162, 235)', /*bleu*/
+      'rgb(255, 205, 86)', /*orange*/
+      'rgb(0, 128, 0)', /*vert*/
+      'rgb(139, 0, 139)', /*violet */
+      'rgb(255, 20, 50)', /*rouge */
         ],
-  
+
       },
+
       {
         type:'doughnut',
-        label:'DigitNumber sachant Vrai',
-        data:[S2,S1],
+        label:'Faux compte, valeur',
+        data:[g1E2,g2E2,g3E2,g4E2,g5E2,g6E2],
         backgroundColor: [
-          'rgb(25, 30, 50)',
-          'rgba(2, 255, 128, .5)',
+          'rgb(255, 99, 132)', /*rose */
+      'rgb(54, 162, 235)', /*bleu*/
+      'rgb(255, 205, 86)', /*orange*/
+      'rgb(0, 128, 0)', /*vert*/
+      'rgb(139, 0, 139)', /*violet */
+      'rgb(255, 20, 50)', /*rouge */
         ],
   
-      },
-  ]
+      }],
+    
   };
   
   return (
     <div>
-      <h2>PolarArea Chart</h2>
+      <h2>Double Doughnut Chart</h2>
       <Pie data={finalData}/>
       <h3> Description</h3>
-      <p>This chart shows the difference in term of DigitNumber in the username in both fake and real accounts</p>
+      <p>This chart shows the difference in term of DigitNumber in the username in both fake and real accounts
+The outer circle represents the distribution of number groups for the real counts in our dataset. The inner circle represents the pattern of digits in user names for fake accounts.
+The maximum number of digits that a username can contain in our dataset is 10. We have therefore separated by six subgroups the number of digits in a username.
+<br></br>This chart was built with the aim of answering the following hypotheses:<br></br>
+H1: Are the two studied variables related<br></br>
+H2: The more digits there are in the username, the more likely that user's account is to be a fake account.<br></br>
+H3: The fewer digits in the username, the more likely that user's account is to be a real account
+
+      </p>
+    
     </div>
   );
 };
