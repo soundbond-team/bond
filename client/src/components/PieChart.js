8bb029d8b42a4f5c8ec8d95b6ae0a6925e3e0175
cathy <cathy.siwatala@gmail.com>
2023-01-20 18:31:37 +0100

    Fusion PieChart et OikarAeraChart

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -12,6 +12,12 @@ const PieChart = () => {
   let nbrFaux = 0;
   let nbrVrai = 0;
   let nbrNum = [];
+  let g1 = [];
+  let g2 = [];
+  let g3 = [];
+  let g4 = [];
+  let g5 = [];
+  let g6 = [];
   
 
   const PYTHON_SERVER_URL = "http://localhost:8000"
@@ -35,41 +41,76 @@ const PieChart = () => {
   }, []);
 
   dataset.map((user) =>{
-      nbrNum.push({x:user.username_digit_count})
-
+     
     if(user.is_fake == 1){
       nbrFaux = nbrFaux+1;
- }
-  if(user.is_fake == 0){
+    }
+
+    if(user.is_fake == 0){
       nbrVrai = nbrVrai+1;
-  }
+    }
+
+    if(user.username_digit_count == 0){
+      g1.push({x:user.username_digit_count})
+    }
+
+    if(user.username_digit_count > 0 && user.username_digit_count < 3){
+      g2.push({x:user.username_digit_count})
+    }
+
+    if(user.username_digit_count > 2 && user.username_digit_count < 5){
+      g3.push({x:user.username_digit_count})
+    }
+
+    if(user.username_digit_count > 5 && user.username_digit_count < 7){
+      g4.push({x:user.username_digit_count})
+    }
+
+    if(user.username_digit_count > 6 && user.username_digit_count < 9){
+      g5.push({x:user.username_digit_count})
+
+    }
+
+    if(user.username_digit_count > 8 && user.username_digit_count < 11){
+      g6.push({x:user.username_digit_count})
+      
+    }
+
   })
+
+  console.log(g1)
+  console.log(g2)
+  console.log(g3)
+  console.log(g4)
+  console.log(g5)
+  console.log(g6)
 
 
 
   const finalData = {
     labels: [
-      'FakeAccountsCount',
-      'RealAccountsCount',
+      'Red',
+      'Green',
+      'Yellow',
+      'Grey',
+      'Blue',
+      'Six',
     ],
     datasets: [
       {
-        type:'polarArea',
-        label:'DigitNumber',
-        data:[30,80],
-        borderColor: 'rgb(255, 205, 86)',
-  
+        type: 'pie',
+        label: ' AccountsCount',
+        data: [g1, g2, g3, g4, g5, g6],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)',
+      'rgb(255, 255, 128)',
+      'rgb(255, 30, 50)',
+      'rgb(255, 20, 50)',
+        ],
+        hoverOffset: 4
       },
-      {
-      type: 'pie',
-      label: ' AccountsCount',
-      data: [nbrFaux, nbrVrai],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-      ],
-      hoverOffset: 4
-    },
   ]
   };
   
