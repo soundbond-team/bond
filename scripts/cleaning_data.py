fc3f0eeefd1c3673e6d4d84a6e089b65108c1947
Maguitadiop <diopma221@gmail.com>
2023-01-31 19:20:43 +0100

    maj

--- scripts/cleaning_data.py
+++ scripts/cleaning_data.py
@@ -14,10 +14,24 @@ data = transform_json_to_dataframe(dataset_path)
 
 #cleaning data
 def clean_data(data):
-    data = data.dropna()
+    data = data.dropna() #dropna supprime les lignes avec des valeurs manquantes
     return data
 
 data_clean = clean_data(data)
 
 print(data_clean)
+
+#export data cleaning to json
+def export_data_to_json_orient(data_clean):
+    data_clean.to_json("./data/accountData_clean.json", orient="records")
+
+export_data_to_json_orient(data_clean)
+
+#compare data before and after cleaning
+def compare_data(data, data_clean):
+    print("Data before cleaning: ", data.shape)
+    print("Data after cleaning: ", data_clean.shape)
+
+compare_data(data, data_clean)
+
 
